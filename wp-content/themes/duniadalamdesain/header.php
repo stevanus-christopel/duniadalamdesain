192fed5a356f7480cbe9cd9a3db2bfda5ec137ba
Stevanus Christopel <me@stevanus.id>
2018-10-30 09:08:49 +0700

    New Themes duniadalamdesain

---
+++ wp-content/themes/duniadalamdesain/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="title" content="Dunia Dalam Desain" />
+	<meta name="description" content="Dunia Dalam Desain">
+	<meta name="keywords" content="desain" />
+	<meta name="author" content="Stevanus Christopel - http://stevanus.id">
+	<meta property="og:title" content="Dunia Dalam Desain"/>
+	<meta property="og:description" content="Dunia Dalam Desain"/>
+	<meta property="og:site_name" content="Dunia Dalam Desain"/>
+	<meta property="og:url" content="http://www.duniadalamdesain.com/"/>
+	<meta name="geo.region" content="ID-JI" />
+	<meta name="geo.placename" content="Jakarta" />
+	<meta name="geo.position" content="-7.275995;112.70108" />
+	<meta name="revisit-after" content="7" />
+	<meta name="webcrawlers" content="all" />
+	<meta name="rating" content="general" />
+	<meta name="spiders" content="all"  />
+	<meta name="rating" content="general" />
+	<meta name="spiders" content="all"  />
+	
+	<!-- <link rel="shortcut icon" href="favicon.ico"> -->
+  <title>Dunia Dalam Desain</title>
+
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/styles/reset.css'; ?>">
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+</head>
+
+<body>
---
+++ wp-content/themes/duniadalamdesain/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1>DDD - Dunia Dalam Desain</h1>
+
+<?php get_footer(); ?>
